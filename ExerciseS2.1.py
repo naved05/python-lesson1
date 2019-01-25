72e8a1d39c961a93ec311732df0f23c9457f7c2e
naved05 <40913025+naved05@users.noreply.github.com>
2019-01-26 00:06:39 +0530

    Add files via upload

---
+++ ExerciseS2.1.py
@@ -0,0 +1,11 @@
+inital_km=100
+final_km=200
+
+fuel_filled=input("enter the fuel tank capacity:")
+
+fuel=int(fuel_filled)
+
+milleage=(final_km-inital_km)/fuel
+
+print("milage of the car is",milleage)
+
---
+++ ExerciseS2.2.py
@@ -0,0 +1,9 @@
+distance= 560 
+
+fuel_filled=input("enter the fuel tank capacity:")
+
+fuel=int(fuel_filled)
+
+refilling= distance/fuel
+
+print("number of timed refilling required",refilling)
